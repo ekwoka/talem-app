355fc780a815952354a2bcc13e9a343601674cb9
PkTheCoda <pranavkonjeti@gmail.com>
2023-07-05 11:35:42 -0400

    Fixed header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import logo from './logo.svg';
 import './index.css';
+import Header from './Components/Header';
+
 
 function App() {
   return (
-    <div className=''>
-      Hello Worldw
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
 
