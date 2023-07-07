262d44ce64baa1e1930cf09fd84f9bf1651fdfd3
PkTheCoda <pranavkonjeti@gmail.com>
2023-07-07 16:23:37 -0400

    Fixed styling for EC item and added loading marker

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Homepage from './Pages/Homepage';
 import Extracurriculars from './Pages/Extracurriculars';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import { initializeApp } from 'firebase/app'
-import {getFirestore, collection, getDocs, getDoc} from 'firebase/firestore'
+import { getFirestore, collection, getDocs, getDoc } from 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: "AIzaSyB-R34s08Noa6MHOXTYMH69n8hmJfPflxY",
@@ -19,29 +19,31 @@ const firebaseConfig = {
 
 initializeApp(firebaseConfig)
 
-let extracurricularsArray = []
-
-let db = getFirestore()
-let extracurricularsRef = collection(db, 'extracurriculars')
-getDocs(extracurricularsRef)
-  .then((snapshot) => {
-    snapshot.docs.forEach((doc) => {
-      extracurricularsArray.push({ ...doc.data(), id: doc.id})
-    })
-    console.log(extracurricularsArray)
-  })
-  .catch(err => {
-    console.log(err.message)
-  })
-
-
-
 function App() {
+  const [extracurricularsArray, setExtracurricularsArray] = useState([]);
+
+  useEffect(() => {
+    const fetchExtracurriculars = async () => {
+      try {
+        const db = getFirestore();
+        const extracurricularsRef = collection(db, 'extracurriculars');
+        const snapshot = await getDocs(extracurricularsRef);
+        const data = snapshot.docs.map(doc => ({ ...doc.data(), id: doc.id }));
+        setExtracurricularsArray(data);
+        console.log(data);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+
+    fetchExtracurriculars();
+  }, []);
+
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route index element={<Homepage/>} />
+          <Route index element={<Homepage />} />
           <Route path="/extracurriculars" element={<Extracurriculars ecArray={extracurricularsArray} />} />
         </Routes>
       </BrowserRouter>
