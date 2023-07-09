13c51e878f56e8f0fb2ca83fe345d26fbb5d76a0
PkTheCoda <pranavkonjeti@gmail.com>
2023-07-09 13:42:22 -0400

    fixed styling for search results a bit

--- src/Pages/Extracurriculars.jsx
+++ src/Pages/Extracurriculars.jsx
@@ -564,7 +564,7 @@ export default function Extracurriculars(props) {
                                 </div>
                             }       
 
-                            <div className="pl-10 text-lg pt-4 font-outfit">{props.ecArray.length} Search Results</div>
+                            <div className="ml-10 text-lg mt-4 font-outfit">{props.ecArray.length} Search Results</div>
                             {/* Actual EC Components */}
                             <div className="w-full p-4 px-8 h-max flex-col gap-2 flex justify-center"> {/* Main EC holder */}
                                 {props.ecArray.length === 0 ? (
